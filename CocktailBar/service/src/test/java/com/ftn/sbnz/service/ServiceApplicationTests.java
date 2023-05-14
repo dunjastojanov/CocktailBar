fc0661b35babbe60d5042806533338a63153a755
Dunja <dunjastojanov2@gmail.com>
2023-05-14 02:40:41 +0200

    Initialize project

---
+++ CocktailBar/service/src/test/java/com/ftn/sbnz/service/ServiceApplicationTests.java
@@ -0,0 +1,22 @@
+package com.ftn.sbnz.service;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("com.ftn.sbnz", "kjar", "0.0.1-SNAPSHOT"));
+        KieScanner kScanner = ks.newKieScanner(kContainer);
+        kScanner.start(1000);
+        return kContainer;
+    }
+}
