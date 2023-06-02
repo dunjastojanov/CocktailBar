e446e06217360903ce1e7f31212f77ee297fea69
Dunja <dunjastojanov2@gmail.com>
2023-06-02 10:49:42 +0200

    Added modifiable set

--- CocktailBar/model/src/main/java/com/ftn/sbnz/model/preference/PreferableIngredientList.java
+++ CocktailBar/model/src/main/java/com/ftn/sbnz/model/preference/PreferableIngredientList.java
@@ -1,20 +1,27 @@
 package com.ftn.sbnz.model.preference;
 
 import com.ftn.sbnz.model.cocktail.Ingredient;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PreferableIngredientList {
-    private Set<Ingredient> ingredients = new HashSet<>();
+    private Set<Ingredient> ingredients;
 
     public boolean containsIngredient(Ingredient ingredient) {
         return ingredients.contains(ingredient);
     }
 
     public void addIngredient(Ingredient ingredient){
+        if (ingredients == null) {
+            ingredients = new HashSet<>();
+        }
         ingredients.add(ingredient);
     }
 
